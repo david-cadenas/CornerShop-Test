1c4aade072f3303a4fbb09d858984fa7bbe5b93b
Gustavo <antero105@gmail.com>
2020-04-20 14:19:19 -0400

    Improving model to accept unicode

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.types import TypeDecorator, Unicode
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import UniqueConstraint
@@ -8,6 +9,19 @@ from sqlalchemy.sql.schema import UniqueConstraint
 Base = declarative_base()
 
 
+
+
+class CoerceUTF8(TypeDecorator):
+    """Safely coerce Python bytestrings to Unicode
+    before passing off to the database."""
+
+    impl = Unicode
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, str):
+            value = value.decode('utf-8')
+        return value
+
 class Product(Base):
 
     __tablename__ = 'products'
@@ -16,12 +30,12 @@ class Product(Base):
     store = Column(String, nullable=False)
     barcodes = Column(String, nullable=True)
     sku = Column(String, nullable=True)
-    brand = Column(String, nullable=True)
-    name = Column(String, nullable=False)
-    description = Column(String, nullable=True)
+    brand = Column(CoerceUTF8, nullable=True)
+    name = Column(CoerceUTF8, nullable=False)
+    description = Column(CoerceUTF8, nullable=True)
     package = Column(String, nullable=True)
     image_urls = Column(String, nullable=True)
-    categories = Column(String, nullable=True)
+    categories = Column(CoerceUTF8, nullable=True)
 
     branch_products = relationship("BranchProduct", back_populates="product")
 
